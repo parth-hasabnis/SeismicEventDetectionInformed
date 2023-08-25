4a359142d2e2297e35260cb40cd063afb28d89dc
Parth <parth.hasabnis77@gmail.com>
2023-08-25 13:02:33 -0400

    Added args and evaluate.py

---
+++ Utility_files/metrics.py
@@ -0,0 +1,68 @@
+import torch
+
+class metrics():
+
+    def __init__(self, prediction:torch.Tensor, target:torch.Tensor, thresholds) -> None:
+        device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+        self.n_events = target.shape[-1]
+        assert all(val>0 and val<1 for val in thresholds)
+        ones = torch.ones(target.shape)
+        ones = ones.to(device)
+        for i in range(self.n_events):
+            ones[:, i] = ones[:, i] * thresholds[i]
+        ones = prediction > ones
+        self.prediction = ones.float()*1
+        self.origPredictions = self.prediction
+        self.target = target.permute(2, 0, 1)
+        self.prediction = self.prediction.permute(2, 0, 1)
+        
+    def accuracy(self):
+        return torch.sum(torch.eq(self.prediction, self.target))/self.target.nelement()
+    
+    def Errors(self):
+        """
+        Calculate the Different Errors and metrics for the training
+        """
+
+        metrics = {
+            "type 1":[],
+            "type 2":[],
+            "precsion":[],
+            "recall": []
+        }
+
+        for i in range(self.n_events):
+            prediction = self.prediction[i].flatten()
+            target = self.target[i].flatten()
+            FALSE_ALARM_COUNT = 0
+            MISS_COUNT = 0
+            TRUE_POSITIVE_COUNT = 0
+            TRUE_NEGATIVE_COUNT = 0
+
+            for (p,t) in zip(prediction, target):
+                if t == 1 and p == 1:
+                    TRUE_POSITIVE_COUNT = TRUE_POSITIVE_COUNT + 1
+                if t == 0 and p == 0:
+                    TRUE_NEGATIVE_COUNT = TRUE_NEGATIVE_COUNT + 1
+                if t == 1 and p == 0:
+                    MISS_COUNT = MISS_COUNT + 1
+                if t == 0 and p == 1:
+                    FALSE_ALARM_COUNT = FALSE_ALARM_COUNT + 1
+            
+                TYPE_1 = FALSE_ALARM_COUNT/(FALSE_ALARM_COUNT + TRUE_NEGATIVE_COUNT + 0.00001)
+                TYPE_2 = MISS_COUNT/(MISS_COUNT + TRUE_POSITIVE_COUNT + 0.00001)
+                PRECISION = TRUE_POSITIVE_COUNT/(TRUE_POSITIVE_COUNT + FALSE_ALARM_COUNT + 0.00001)
+                RECALL = TRUE_POSITIVE_COUNT/(TRUE_POSITIVE_COUNT + MISS_COUNT + 0.00001)     
+                ACCURACY = TRUE_POSITIVE_COUNT + TRUE_NEGATIVE_COUNT/(TRUE_POSITIVE_COUNT + TRUE_NEGATIVE_COUNT + FALSE_ALARM_COUNT + MISS_COUNT)
+            metrics['type 1'].append(TYPE_1)
+            metrics['type 2'].append(TYPE_2)
+            metrics['precsion'].append(PRECISION)
+            metrics['recall'].append(RECALL)
+
+        return metrics
+    
+    def get_thresholded_predictions(self):
+        """
+        Return the thresholded predictions 
+        """
+        return self.origPredictions
--- evalute.py
+++ evalute.py
@@ -7,9 +7,56 @@ from torch.utils.data import DataLoader
 import numpy as np
 import matplotlib.pyplot as plt
 import json
+import Utility_files.metrics as metrics
+import Utility_files.test_args as test_args
 from Utility_files.create_data import SeismicEventDataset, relabel_dataset, TwoStreamBatchSampler, MultiStreamBatchSampler
 from models.CRNN import CRNN
 from Utility_files.utils import weights_init, sigmoid_rampup, linear_rampup
 import librosa
 import librosa.display
+
+def test_model(weights, save_path, dataset_path, dataset_type, save_spectrograms, plot_ROC, save_metrics):
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    TEST_PATH = dataset_path    # Set dataset path 
+    args = test_args            # Use standard parameters
+
+    seed = 75
+    torch.manual_seed(seed)
+    np.random.seed(seed)
+
+    f = open("Results" + save_path + "/crnnn_args.json")
+    crnn_kwargs = json.loads(f.read())
+    f.close()
+    pooling_time_ratio = 4  # 2 * 2
+
+    crnn = CRNN(**crnn_kwargs)
+    crnn.load_state_dict(torch.load(f"Results/{save_path}/Checkpoints/{model_weights}" ))
+    crnn.to(device)
+    test_dataset = SeismicEventDataset(TEST_PATH, args, dataset_type)
+    eval_loader = DataLoader(test_dataset, args.batch_size, drop_last=False)
+
+    bases = np.linspace(-5, 5, 22)          # Thresholds for predictions
+    thresholds = 1/(1 + np.exp(-bases))     # Generate exponentially distributed thresholds
+
+    if(plot_ROC):
+        yConsolidated = []
+        predictionConsolidated = []    
+
+    for batch, (X,y) in enumerate(eval_loader):
+
+        X = X.to(device)
+        y = y.to(device)
+        prediction = crnn(X)
+
+
+
+
+
+
+if __name__ == "__main__":
+
+    device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+    save_path = "/Aug_21_2023"                                              # path to save training and testing results
+    model_weights = "student_epoch_9.pt"                                    # model to evaluate
 
